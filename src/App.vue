e0067e10ca2d4e105d4ef5030ed842020d29e467
tsrkzy <tsrmix@gmail.com>
2025-07-15 06:37:29 +0900

    biomejs + prettier

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,16 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 <template>
   <header>
-    <img alt="Vue logo"
-         class="logo"
-         src="@/assets/logo.svg" width="125" height="125"/>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -19,7 +23,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
     </div>
   </header>
 
-  <RouterView/>
+  <RouterView />
 </template>
 
 <style scoped>
